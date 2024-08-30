6e1daee0021f461d9197261b43eb0211686b6903
DiogoTGC <dtimoteo292@gmail.com>
2024-08-30 02:06:50 -0300

    Create the model for Contato

---
+++ Models/ContatoModel.cs
@@ -0,0 +1,9 @@
+namespace ControleDeContatos.Models;
+
+public class ContatoModel
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string Email { get; set; }
+    public required string Celular { get; set; }
+}
