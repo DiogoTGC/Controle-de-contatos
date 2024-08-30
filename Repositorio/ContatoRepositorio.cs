5263c063dacc5964e64124087535d1c1d358965a
DiogoTGC <dtimoteo292@gmail.com>
2024-08-30 03:44:34 -0300

    Made the dependency injection and the Adicionar function

--- Repositorio/ContatoRepositorio.cs
+++ Repositorio/ContatoRepositorio.cs
@@ -1,5 +1,22 @@
+using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
 namespace ControleDeContatos.Repositorio;
 
-public class ContatoRepositorio
+public class ContatoRepositorio : IContatoRepositorio
 {
+    private readonly BancoContext _bancoContext;
+
+    public ContatoRepositorio(BancoContext bancoContext)
+    {
+        _bancoContext = bancoContext;
+    }
+
+    public ContatoModel Adicionar(ContatoModel contato)
+    {
+        _bancoContext.Contatos?.Add(contato);
+        _bancoContext.SaveChanges();
+
+        return contato;
+    }
 }
--- Repositorio/IContatoRepositorio.cs
+++ Repositorio/IContatoRepositorio.cs
@@ -1,5 +1,8 @@
+using ControleDeContatos.Models;
+
 namespace ControleDeContatos.Repositorio;
 
 public interface IContatoRepositorio
 {
+    ContatoModel Adicionar(ContatoModel contato);
 }
