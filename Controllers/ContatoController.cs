6986158dee795a44aafc9e0438c196ac60c4bc1a
DiogoTGC <dtimoteo292@gmail.com>
2024-08-30 04:56:52 -0300

    Add the Atualizar function and linked the page to the respective controller

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -20,9 +20,10 @@ public class ContatoController(IContatoRepositorio contatoRepositorio) : Control
         return View();
     }
 
-    public IActionResult Editar()
+    public IActionResult Editar(int id)
     {
-        return View();
+        ContatoModel? contato = _contatoRepositorio.ListarPorId(id);
+        return View(contato);
     }
 
     public IActionResult ApagarConfirmacao()
@@ -34,6 +35,14 @@ public class ContatoController(IContatoRepositorio contatoRepositorio) : Control
     public IActionResult Criar(ContatoModel contato)
     {
         _contatoRepositorio.Adicionar(contato);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Alterar(ContatoModel contato)
+    {
+        _contatoRepositorio.Atualizar(contato);
 
         return RedirectToAction("Index");
     }
--- Repositorio/ContatoRepositorio.cs
+++ Repositorio/ContatoRepositorio.cs
@@ -7,6 +7,12 @@ public class ContatoRepositorio(BancoContext bancoContext) : IContatoRepositorio
 {
     private readonly BancoContext _bancoContext = bancoContext;
 
+    public ContatoModel? ListarPorId(int id)
+    {
+        ContatoModel? contato = _bancoContext.Contatos?.FirstOrDefault(x => x.Id == id);
+        return contato;
+    }
+
     public List<ContatoModel>? BuscarTodos()
     {
         return _bancoContext.Contatos?.ToList();
@@ -18,5 +24,24 @@ public class ContatoRepositorio(BancoContext bancoContext) : IContatoRepositorio
         _bancoContext.SaveChanges();
 
         return contato;
+    }
+
+    public ContatoModel Atualizar(ContatoModel contato)
+    {
+        ContatoModel? contatoDB = ListarPorId(contato.Id);
+
+        if (contatoDB != null)
+        {
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email = contato.Email;
+            contatoDB.Celular = contato.Celular;
+
+            _bancoContext.Contatos?.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
+
+        throw new Exception("Houve um erro na atualização do contato");
     }
 }
