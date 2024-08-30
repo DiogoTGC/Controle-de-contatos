69b37275bf17dbda38f686a715f8351c03a14610
DiogoTGC <dtimoteo292@gmail.com>
2024-08-30 00:56:46 -0300

    Add ContatoController

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers;
+
+public class ContatoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
