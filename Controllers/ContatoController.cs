2d998562fc183543062be35c12df1c75429460cc
DiogoTGC <dtimoteo292@gmail.com>
2024-08-30 01:17:03 -0300

    Added the get functions in the contato controller to create, edit and delete a Contato

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -8,4 +8,19 @@ public class ContatoController : Controller
     {
         return View();
     }
+
+    public IActionResult Criar()
+    {
+        return View();
+    }
+
+    public IActionResult Editar()
+    {
+        return View();
+    }
+
+    public IActionResult Apagar()
+    {
+        return View();
+    }
 }
