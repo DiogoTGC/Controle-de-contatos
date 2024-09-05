0f50841bf173f058bd0abd111c19ccae68e99283
DiogoTGC <dtimoteo292@gmail.com>
2024-09-04 22:11:31 -0300

    Made the controller functions and changed the model message

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,98 @@
+using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers;
+
+public class UsuarioController(IUsuarioRepositorio usuarioRepositorio) : Controller
+{
+    private readonly IUsuarioRepositorio _usuarioRepositorio = usuarioRepositorio;
+    public IActionResult Index()
+    {
+        List<UsuarioModel>? usuarios = _usuarioRepositorio.BuscarTodos();
+
+        return View(usuarios);
+    }
+
+    public IActionResult Criar()
+    {
+        return View();
+    }
+
+    public IActionResult Editar(int id)
+    {
+        UsuarioModel? usuario = _usuarioRepositorio.ListarPorId(id);
+        return View(usuario);
+    }
+
+    public IActionResult ApagarConfirmacao(int id)
+    {
+        UsuarioModel? usuario = _usuarioRepositorio.ListarPorId(id);
+        return View(usuario);
+    }
+
+    [HttpPost]
+    public IActionResult Criar(UsuarioModel usuario)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.Adicionar(usuario);
+                TempData["MensagemSucesso"] = "Usuario criado com sucesso";
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+        catch (Exception erro)
+        {
+            TempData["MensagemErro"] = $"Falha ao cadastrar o usuario, tente novamente. Detalhe do erro: {erro.Message}";
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Alterar(UsuarioModel usuario)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.Atualizar(usuario);
+                TempData["MensagemSucesso"] = "Usuario alterado com sucesso";
+                return RedirectToAction("Index");
+            }
+
+            return View("Editar", usuario);
+        }
+        catch (Exception erro)
+        {
+            TempData["MensagemErro"] = $"Falha ao alterar o usuario, tente novamente. Detalhe do erro: {erro.Message}";
+            return RedirectToAction("Index");
+        }
+    }
+
+    public IActionResult Apagar(int id)
+    {
+        try
+        {
+            bool apagado = _usuarioRepositorio.Apagar(id);
+
+            if (apagado)
+            {
+                TempData["MensagemSucesso"] = "Usuario apagado com sucesso";
+            }
+            else
+            {
+                TempData["MensagemErro"] = $"Falha ao apagar o usuario, tente novamente.";
+            }
+            return RedirectToAction("Index");
+        }
+        catch (Exception erro)
+        {
+            TempData["MensagemErro"] = $"Falha ao apagar o usuario, tente novamente. Detalhe do erro: {erro.Message}";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Models/UsuarioModel.cs
+++ Models/UsuarioModel.cs
@@ -6,13 +6,15 @@ namespace ControleDeContatos.Models;
 public class UsuarioModel
 {
     public int Id { get; set; }
-    [Required(ErrorMessage = "Digite o nome do contato")]
+    [Required(ErrorMessage = "Digite o nome do usuário")]
     public required string Nome { get; set; }
+    [Required(ErrorMessage = "Digite o login do usuário")]
     public required string Login { get; set; }
-    [Required(ErrorMessage = "Digite o email do contato")]
+    [Required(ErrorMessage = "Digite o email do usuário")]
     [EmailAddress(ErrorMessage = "Email inválido")]
     public required string Email { get; set; }
     public PerfilEnum Perfil { get; set; }
+    [Required(ErrorMessage = "Digite a senha do usuário")]
     public required string Senha { get; set; }
     public DateTime DataCadastro { get; set; }
     public DateTime? DataAtualizacao { get; set; }
