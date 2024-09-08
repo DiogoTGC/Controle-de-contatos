91a562bd5efc4c90d2083c8993b743c8b314e00b
DiogoTGC <dtimoteo292@gmail.com>
2024-09-08 19:16:29 -0300

    Develop the page and the controller when an unauthurized user tries to access a restricted page

---
+++ Controllers/RestritoController.cs
@@ -0,0 +1,13 @@
+using ControleDeContatos.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers;
+
+[PaginaParaUsuarioLogado]
+public class RestritoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
