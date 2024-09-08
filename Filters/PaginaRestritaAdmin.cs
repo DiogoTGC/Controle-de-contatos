e3ae960cbc62bc9f8e4b2fac25ca0730c4b69464
DiogoTGC <dtimoteo292@gmail.com>
2024-09-08 19:12:42 -0300

    Create a filter for users accessing the pages

---
+++ Filters/PaginaRestritaAdmin.cs
@@ -0,0 +1,35 @@
+using System.Text.Json;
+using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ControleDeContatos.Filters;
+
+public class PaginaRestritaAdmin : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        string? sessaoUsuario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+        if (string.IsNullOrEmpty(sessaoUsuario))
+        {
+            context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+        }
+        else
+        {
+            UsuarioModel? usuario = JsonSerializer.Deserialize<UsuarioModel>(sessaoUsuario);
+
+            if (usuario == null)
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+            }
+
+            if (usuario?.Perfil != Enums.PerfilEnum.Admin)
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Restrito" }, { "action", "Index" } });
+            }
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
