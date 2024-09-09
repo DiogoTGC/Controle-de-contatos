cf941f9803cc4ca1a7b3c7c42b62e062c99a2162
DiogoTGC <dtimoteo292@gmail.com>
2024-09-09 17:18:49 -0300

    Implemented an email interface and class and the configs for a SMTP server

---
+++ Helper/Email.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using System.Net.Mail;
+
+namespace ControleDeContatos.Helper;
+
+public class Email(IConfiguration configuration) : IEmail
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public bool Enviar(string email, string assunto, string mensagem)
+    {
+        try
+        {
+            string? host = _configuration.GetValue<string>("SMTP: Host");
+            string? nome = _configuration.GetValue<string>("SMTP: Nome");
+            string? username = _configuration.GetValue<string>("SMTP: UserName");
+            string? senha = _configuration.GetValue<string>("SMTP: Senha");
+            int porta = _configuration.GetValue<int>("SMTP: Porta");
+
+            MailMessage mail = new()
+            {
+                From = new MailAddress(username, nome)
+            };
+
+            mail.To.Add(email);
+            mail.Subject = assunto;
+            mail.Body = mensagem;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+
+            using SmtpClient smtp = new(host, porta);
+            smtp.Credentials = new NetworkCredential(username, senha);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
